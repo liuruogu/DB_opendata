2c7ad5e7be5466f5ca6ed74fd99134eea3f62ac9
liuruogu <liuruogu1994@gmail.com>
2017-05-13 06:37:09 +0200

    Download the data

--- CsvCombind.py
+++ CsvCombind.py
@@ -2,7 +2,9 @@ __author__ = 'liu'
 import pandas as pd
 import io
 import requests
+import os
 
+os.path.getsize()
 
 # Load the data into dataframe
 def LoadCsv(FileName):
@@ -18,8 +20,17 @@ def CombineCsv(FirstCsv,SecondCsv):
     return CombinedCsv
 
 def main():
-    FirstCsv = 'first.csv'
-    firstCsv = LoadCsv(FirstCsv)
+    # FirstCsv = LoadCsv('0002.csv')
+    # SecondCsv = LoadCsv('0003.csv')
+    # CombineCsv(FirstCsv,SecondCsv)
+
+    # firstCsv = LoadCsv("first.csv")
+    # for i in range(1,5):
+    #     print(i)
+    #     print("000"+i+".csv")
+    #     firstCsv = LoadCsv("first.csv")
+    #     # first = pd.read_csv("000"+i+".csv")
+    #     CombineCsv(firstCsv,first)
 
     x = '00'
     y = '00'
@@ -35,12 +46,13 @@ def main():
                 while l<10:
                     filename = str(int(x[0])+i) + str(int(x[1]) + j) + str(int(y[0])+k) + str(int(y[1]) + l)
                     print(filename)
-                    if filename != '0001':
-                        url="https://trnet.cloudrail.jp/hackathon/self_train/0420/0/"+filename+".csv"
-                        s=requests.get(url).content
-                        c=pd.read_csv(io.StringIO(s.decode('utf-8')))
-                        c.to_csv(filename+".csv", sep='\t', encoding='utf-8')
-                        CombineCsv(firstCsv,c)
+                    # if filename != '0001':
+                    url="https://trnet.cloudrail.jp/hackathon/self_train/0421/0/"+filename+".csv"
+                    s=requests.get(url).content
+                    c=pd.read_csv(io.StringIO(s.decode('utf-8')))
+                    c.to_csv(filename+".csv", sep=',', encoding='utf-8')
+                    # LoadCsv(c)
+                    # CombineCsv(firstCsv,c)
                     l=l+1
                 k=k+1
             j=j+1
